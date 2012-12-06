098fb70a86c279b6812690d85193ee9c7f8b15bf
Jakob Hansen <Jakob Hansen@Hansen-Desktop>
2012-12-06 07:23:23 +0100

    Dokumenteret!

--- src/BE/Song.java
+++ src/BE/Song.java
@@ -15,6 +15,11 @@ public class Song
     private String filename;
     private int duration;
     
+    /**
+     * Constructor for a song-object, accepting;
+     * ID and an already existing song-object..
+     * @param id and song object.
+     */
     public Song(int id, Song s)
     {
         this.id = id;
@@ -25,6 +30,11 @@ public class Song
         this.filename = s.getFilename();
     }
     
+    /**
+     * Constructor for a song-object, accepting;
+     * Title, Artist, Category and Filename.
+     * @param title, artist, category and filename.
+     */
     public Song(String title, String artist, String category, String filename)
     {
         this.title = title;
@@ -33,6 +43,11 @@ public class Song
         this.filename = filename;
     }
     
+    /**
+     * Constructor for a song-object, accepting;
+     * ID, Title, Artist, Category, Filename and Duration.
+     * @param id, title, artist, category, filename and duration.
+     */
     public Song(int id, String title , String artist, String category, String filename, int duration)
     {
         this.id = id;
@@ -44,7 +59,8 @@ public class Song
     }
 
     /**
-     * @return the id
+     * Returns the ID of a song.
+     * @return the id.
      */
     public int getId()
     {
@@ -52,7 +68,8 @@ public class Song
     }
 
     /**
-     * @return the name
+     * Returns the title of a song.
+     * @return the title.
      */
     public String getTitle()
     {
@@ -60,7 +77,8 @@ public class Song
     }
 
     /**
-     * @param name the name to set
+     * Sets the title of a song.
+     * @param name the title to set.
      */
     public void setTitle(String title)
     {
@@ -68,7 +86,8 @@ public class Song
     }
 
     /**
-     * @return the address
+     * Returns the artist linked to a song.
+     * @return the artist.
      */
     public String getArtist()
     {
@@ -76,7 +95,8 @@ public class Song
     }
 
     /**
-     * @param address the address to set
+     * Set the artist of a song.
+     * @param address the artist.
      */
     public void setArtist(String artist)
     {
@@ -84,7 +104,8 @@ public class Song
     }
 
     /**
-     * @return the depNum
+     * Returns the category of a song.
+     * @return the category.
      */
     public String getCategory()
     {
@@ -92,39 +113,55 @@ public class Song
     }
 
     /**
-     * @param depNum the depNum to set
+     * Sets the category of a song.
+     * @param depNum the category to set.
      */
     public void setCategory(String category)
     {
         this.category = category;
     }
     
+     /**
+     * Returns the duration of a song.
+     * @return the duration.
+     */
     public int getDuration()
     {
         return duration;
     }
     
+    /**
+     * Sets the duration of a song.
+     * @param depNum the duration to set.
+     */
     public void setDuration(int duration)
     {
         this.duration = duration;
     }
     
+    /**
+     * Returns the filename of a song.
+     * @return the filename.
+     */
     public String getFilename()
     {
         return filename;
     }
     
+    /**
+     * Sets the filename of a song.
+     * @param depNum the filename to set.
+     */
     public void setFilename(String filename)
     {
         this.filename = filename;
     }
-    
 
-    @Override
-    public String toString()
-    {
-        //int min = duration/60;
-        //int sec = duration-min*60;
-        return String.format("%3d %-30s %-30s %4d %2d", title, artist, category, duration, id);
-    }
+//    @Override
+//    public String toString()
+//    {
+//        //int min = duration/60;
+//        //int sec = duration-min*60;
+//        return String.format("%3d %-30s %-30s %4d %2d", title, artist, category, duration, id);
+//    }
 }
